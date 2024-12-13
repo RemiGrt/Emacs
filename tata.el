f49360509e37df86c176bc1f7b671e9107eb99a9
RemiGrt <remi.griot@gmail.com>
2024-12-13 14:51:13 +0100

    assac

---
+++ tata.el
@@ -0,0 +1,8 @@
+defun how-many-str (regexp str)
+  (loop with start = 0
+        for count from 0
+        while (string-match regexp str start)
+        do (setq start (match-end 0))
+        finally return count))
+
+(directory-files "." t ".txt$")
