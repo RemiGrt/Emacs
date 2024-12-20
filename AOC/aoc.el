e1ac5a53fd4496c3e968d57a280f4343e2b522b5
RemiGrt <remi.griot@gmail.com>
2024-12-20 23:55:22 +0100

    add aoc

---
+++ AOC/aoc.el
@@ -0,0 +1,16 @@
+(let ((bags (with-temp-buffer
+  (progn (insert "((")
+	 (insert-file-contents "./input.txt")
+	 (while (re-search-forward "^$" nil t)
+	   (replace-match ")(" nil t))
+	 (end-of-buffer)
+	 (insert "))")
+	 (goto-char 0))
+  (read (current-buffer)))))
+  (cl-reduce #'max
+	     (mapcar (lambda (bag)
+		       (cl-reduce #'+ bag))
+		     bags
+		     )))
+
+
