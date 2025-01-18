534a756af1339125a51eb5250ca143ebf5e7e568
RemiEfrei <remi.griot@efrei.fr>
2025-01-18 16:04:42 +0100

    up

--- 99_problems.el
+++ 99_problems.el
@@ -5,7 +5,19 @@ Example:
 * (my-last '(a b c d))
 (D)"
 
+(setq a 3)
+
+(1+ a)
+
+(list "a" "b" "c")
+
+;; 1
+
 (defun rg-last (list)
+  "Find the last box of a list.
+Example:
+* (my-last '(a b c d))
+(D)"
   (when list
     (if (cdr list)
 	(rg-last (cdr list))
@@ -13,4 +25,37 @@ Example:
     )
   )
 
-(rg-last '(a b c d))
+;; 2
+
+(defun rg-but-last (list)
+  "Find the last but one box of a list.
+Example:
+* (my-but-last '(a b c d))
+(C D)"
+  (when list
+    (if (cddr list)
+	(rg-but-last (cddr list))
+      list
+      )
+    )
+  )
+
+(rg-but-last '(a b c d))
+		     
+
+;; 3
+(defun rg-element-at (list k)
+  "Find the K'th element of a list.
+The first element in the list is number 1.
+Example:
+* (element-at '(a b c d e) 3)
+C"
+  (cond (
+	 (= k 1) (car list)
+	 )
+        (
+	 list (element-at (cdr list) (1- k)))
+	)
+  )
+
+(rg-element-at '(1 2 3) 2)
